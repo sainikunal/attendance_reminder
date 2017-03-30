bd6db994f2a4d061821e912b25fe03f1b2280946
sainikunal <hackerzdiary@gmail.com>
2017-03-30 22:31:25 +0530

    removed __name__ == __main__

--- faculty_class.py
+++ faculty_class.py
@@ -221,9 +221,4 @@ class Faculty:
         driver.close()
         
 
-if __name__ == "__main__":
-    username = raw_input("Username : ")
-    password = getpass.getpass()
-    faculty = Faculty(username, password)
-    faculty.first_time_login()
-    faculty.download_attendance_report()
+
